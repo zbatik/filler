a827762afcceb2f524772f7e9a986712540c9244
Zack BATIK <zbatik@e4r21p4.wethinkcode.co.za>
2018-07-27 15:12:20 +0200

    run bulk tests

--- resources/bulk_test.sh
+++ resources/bulk_test.sh
@@ -6,7 +6,7 @@
 #    By: zbatik <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/07/23 17:37:41 by zbatik            #+#    #+#              #
-#    Updated: 2018/07/23 18:24:05 by zbatik           ###   ########.fr        #
+#    Updated: 2018/07/24 15:15:11 by zbatik           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@ play_filler () {
 	echo PLAYING\ AS\ P$1\ AGAINSTS\ $3\ ON\ MAP\ $4 >> result.log
 	for count in {1..5}
 	do
-		./filler_vm -p$1 ./zbatik.filler -p$2 ./players/$3.filler -f ./maps/map0$4
+		./filler_vm -p$1 ./zbatik.filler -p$2 ./players/$3.filler -q -f ./maps/map0$4
 		cat filler.trace | sed -n '2p' >> result.log
 	done
 }
@@ -26,6 +26,9 @@ players='carli grati champely hcao superjeannot abanlin'
 
 for player in $players
 do
-	play_filler 1 2 $player 0
-	play_filler 2 1 $player 0
+	for	map in {0..2}
+	do
+		play_filler 1 2 $player $map
+		play_filler 2 1 $player $map
+	done
 done
