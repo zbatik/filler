e4388a446b550f5894c14f9b2fad4b9c936519d9
Zack BATIK <zbatik@e4r21p4.wethinkcode.co.za>
2018-07-30 11:55:30 +0200

    push ready

--- resources/run_tests.sh
+++ resources/run_tests.sh
@@ -6,16 +6,24 @@
 #    By: zbatik <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/07/23 17:37:41 by zbatik            #+#    #+#              #
-#    Updated: 2018/07/30 11:11:06 by zbatik           ###   ########.fr        #
+#    Updated: 2018/07/30 11:18:06 by zbatik           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 play_filler () {
 	echo PLAYING\ AS\ P$1\ AGAINSTS\ $3\ ON\ MAP\ $4 >> result.log
+	wins=0
 	for count in {1..5}
 	do
 		./filler_vm -p$1 ./zbatik.filler -p$2 ./players/$3.filler -q -f ./maps/map0$4
-		cat filler.trace | sed -n '2p' >> result.log
+		winner=$(sed -n '2p' filler.trace)
+		echo $winner >> result.log
+		if [ "$winner" == "./zbatik.filler won" ]; then
+			wins=$(echo $wins + 1 | bc)
+		fi
+		if [ $wins == 3 ]; then
+			break
+		fi
 	done
 }
 
@@ -24,11 +32,12 @@ touch result.log
 
 players='champely abanlin hcao grati carli superjeannot'
 
-for player in $players
+for	map in {0..2}
 do
-	for	map in {0..2}
-	do
-		play_filler 1 2 $player $map
-		play_filler 2 1 $player $map
-	done
+	play_filler 1 2 champely $map
+	play_filler 2 1 abanlin $map
+	play_filler 2 1 hcao $map
+	play_filler 2 1 grati $map
+	play_filler 1 2 carli $map
+	play_filler 1 2 superjeannot $map
 done
---
+++ resources/test.sh
@@ -0,0 +1,9 @@
+
+p1=$((1 + $RANDOM % 2))
+if [ $p1  == 1 ]; then
+	p2=2
+else 
+   	p2=1
+fi
+ echo $p1
+ echo $p2
